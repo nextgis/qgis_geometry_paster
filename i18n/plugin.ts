3a64002abd9b8394e5c16596e27db7841b6627c8
Maxim Dubinin <sim@gis-lab.info>
2018-12-13 21:15:20 +0300

    Update plugin.ts

--- i18n/plugin.ts
+++ i18n/plugin.ts
@@ -15,47 +15,47 @@
     <message>
         <location filename="../plugin.py" line="127"/>
         <source>Fail to paste. Multiple features in the clipboard.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка вставки. В буфере несколько объектов.</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="134"/>
         <source>Nothing to paste. No features in the clipboard.</source>
-        <translation type="unfinished"></translation>
+        <translation>Нечего вставлять. Буфер пуст.</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="147"/>
         <source>Multiple features are selected. There should be only one.</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрано несколько объектов, но нужен только один.</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="156"/>
         <source>Nowhere to paste. No target feature selected.</source>
-        <translation type="unfinished"></translation>
+        <translation>Некуда вставлять. Не выбран ни один объект.</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="166"/>
         <source>Incompatible geometries. Trying to paste %s to %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Несовместимые геометрии. Попытка вставки %s в %s</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="179"/>
         <source>Something is wrong. Can&apos;t change geometry.</source>
-        <translation type="unfinished"></translation>
+        <translation>Что-то пошло не так. Не удалось изменить геометрию.</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="235"/>
         <source>Select a target feature!</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите целевой объект!</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="238"/>
         <source>Start editing a vector layer!</source>
-        <translation type="unfinished"></translation>
+        <translation>Начните редактировать целевой слой!</translation>
     </message>
     <message>
         <location filename="../plugin.py" line="243"/>
         <source>Copy feature with the geometry you need to paste first!</source>
-        <translation type="unfinished"></translation>
+        <translation>Сначала скопируйте объект с геометрией которую нужно вставить!</translation>
     </message>
 </context>
 </TS>
