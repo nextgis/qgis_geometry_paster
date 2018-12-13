40411fb92a805ce17f93e7afac251d110ecef058
Alexander Lisovenko <alexander.lisovenko@gmail.com>
2018-12-13 21:07:21 +0300

    Generate .ts file

--- i18n/plugin.ts
+++ i18n/plugin.ts
@@ -3,43 +3,58 @@
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="../plugin.py" line="85"/>
+        <location filename="../plugin.py" line="250"/>
         <source>Paste Geometry</source>
         <translation>Вставить геометрию</translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="186"/>
+        <location filename="../plugin.py" line="179"/>
         <source>Paste geometry</source>
         <translation>Вставить геометрию</translation>
     </message>
     <message>
+        <location filename="../plugin.py" line="127"/>
+        <source>Fail to paste. Multiple features in the clipboard.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../plugin.py" line="134"/>
-        <source>There are many features in clipboard</source>
+        <source>Nothing to paste. No features in the clipboard.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="141"/>
-        <source>Nothing to paste. There is no feature in clipboard</source>
+        <location filename="../plugin.py" line="147"/>
+        <source>Multiple features are selected. There should be only one.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="154"/>
-        <source>Multiple features are selected.</source>
+        <location filename="../plugin.py" line="156"/>
+        <source>Nowhere to paste. No target feature selected.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="163"/>
-        <source>Nothing for paste. There is no one suitable feature</source>
+        <location filename="../plugin.py" line="166"/>
+        <source>Incompatible geometries. Trying to paste %s to %s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="173"/>
-        <source>Attempting to write %s to %s</source>
+        <location filename="../plugin.py" line="179"/>
+        <source>Something is wrong. Can&apos;t change geometry.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugin.py" line="186"/>
-        <source>Something wrong. Cann&apos;t change geometry</source>
+        <location filename="../plugin.py" line="235"/>
+        <source>Select a target feature!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="238"/>
+        <source>Start editing a vector layer!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="243"/>
+        <source>Copy feature with the geometry you need to paste first!</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
