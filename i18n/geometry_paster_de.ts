510da72145e4056abbaaa742967230794353c310
Florian Jenn <florian.jenn@gci-kw.de>
2024-05-29 13:38:41 +0200

    German translation

---
+++ i18n/geometry_paster_de.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>AboutDialogBase</name>
+    <message>
+        <location filename="../about_dialog_base.ui" line="14"/>
+        <source>About {plugin_name}</source>
+        <translation>Über {plugin_name}</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog_base.ui" line="27"/>
+        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
+        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog_base.ui" line="40"/>
+        <source>&lt;p&gt;{description}&lt;/p&gt;
+&lt;p&gt;{about}&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}/{utm}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
+{video_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;Other helpful services by NextGIS:
+&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
+&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
+&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
+&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
+{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
+&lt;/p&gt;</source>
+        <translation>&lt;p&gt;{description}&lt;/p&gt;
+&lt;p&gt;{about}&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Entwickler:&lt;/b&gt; &lt;a href=&quot;{main_url}/{utm}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Video mit Überblick über das Plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
+{video_url}&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;&lt;b&gt;Fehler bitte im &lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;Bugtracker&lt;/a&gt; melden&lt;/p&gt;
+&lt;p&gt;Andere hilfreiche Dienste von NextGIS:
+&lt;ul&gt;&lt;li&gt;&lt;b&gt;Praktische aktuelle Datenauszüge für jeden Ort der Welt:
+&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
+&lt;li&gt;&lt;b&gt;Kompletter Web-GIS-Dienst:
+&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
+{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
+&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../plugin.py" line="254"/>
+        <source>Paste Geometry</source>
+        <translation>Geometrie einfügen</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="195"/>
+        <source>Paste geometry</source>
+        <translation>Geometrie einfügen</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="144"/>
+        <source>Fail to paste. Multiple features in the clipboard.</source>
+        <translation>Konnte nicht einfügen: mehrere Objekte in der Zwischenablage.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="151"/>
+        <source>Nothing to paste. No features in the clipboard.</source>
+        <translation>Nichts einzufügen:keine Objekte in der Zwischenablage.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="164"/>
+        <source>Multiple features are selected. There should be only one.</source>
+        <translation>Mehrere Objekte sind ausgewählt. Es sollte nur eines sein.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="173"/>
+        <source>Nowhere to paste. No target feature selected.</source>
+        <translation>Kann nirgendwo einfügen. Es ist  kein Zielobjekt ausgewählt.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="181"/>
+        <source>Incompatible geometries. Trying to paste %s to %s</source>
+        <translation>Unverträgliche Geometrien: versuche, %s in %s einzufügen.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="195"/>
+        <source>Something is wrong. Can&apos;t change geometry.</source>
+        <translation>Es gab ein Problem; kann die Geometrie nicht ändern.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="239"/>
+        <source>Select a target feature!</source>
+        <translation>Wähl ein Zielobjekt!</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="242"/>
+        <source>Start editing a vector layer!</source>
+        <translation>Starte die Bearbeitung eines Vektorlayers!</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="247"/>
+        <source>Copy feature with the geometry you need to paste first!</source>
+        <translation>Kopier das Objekt mit der Geometrie, die Du benötigst!</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="121"/>
+        <source>Geometry Paster</source>
+        <translation>Geometrie-Einfüger</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="100"/>
+        <source>About plugin…</source>
+        <translation>Über das Plugin…</translation>
+    </message>
+</context>
+</TS>
