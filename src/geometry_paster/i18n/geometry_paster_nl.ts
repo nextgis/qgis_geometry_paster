15bcd9b9d9483888a90fc506fdc6d78ed1b8aec7
Michel Stuyts <mstuyts@users.noreply.github.com>
2024-06-26 10:27:32 +0200

    Added Dutch translation

---
+++ src/geometry_paster/i18n/geometry_paster_nl.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.py" line="82"/>
+        <source>Developers</source>
+        <translation>Ontwikkelaars</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="83"/>
+        <source>Homepage</source>
+        <translation>Startpagina</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="84"/>
+        <source>Please report bugs at</source>
+        <translation>Meld bugs op de</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="87"/>
+        <source>Video with an overview of the plugin</source>
+        <translation>Video met een overzicht van de plugin</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="88"/>
+        <source>Other helpful services by NextGIS</source>
+        <translation>Andere nuttige services van NextGIS</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="89"/>
+        <source>Convenient up-to-date data extracts for any place in the world</source>
+        <translation>Praktische actuele gegevens voor elke plek ter wereld</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="92"/>
+        <source>Fully featured Web GIS service</source>
+        <translation>Complete Web GIS service</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="77"/>
+        <source>REPORT_END</source>
+        <translation>REPORT_END</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="86"/>
+        <source>bugtracker</source>
+        <translation>bugtracker</translation>
+    </message>
+</context>
+<context>
+    <name>AboutDialogBase</name>
+    <message>
+        <location filename="../about_dialog_base.ui" line="14"/>
+        <source>About {plugin_name}</source>
+        <translation>Over {plugin_name}</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog_base.ui" line="27"/>
+        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
+        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <location filename="../plugin.py" line="234"/>
+        <source>Paste Geometry</source>
+        <translation>Plak geometrie</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="179"/>
+        <source>Paste geometry</source>
+        <translation>Plak geometrie</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="133"/>
+        <source>Fail to paste. Multiple features in the clipboard.</source>
+        <translation>Kon niet plakken: meerdere objecten op het klembord.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="140"/>
+        <source>Nothing to paste. No features in the clipboard.</source>
+        <translation>Niets om te plakken: Geen objecten op het klembord.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="153"/>
+        <source>Nowhere to paste. No target feature selected.</source>
+        <translation>Kan nergens naar plakken.  Geen doelobject geselecteerd.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="161"/>
+        <source>Incompatible geometries. Trying to paste %s to %s</source>
+        <translation>Incompatibele geometrieën: Poging om %s te plakken in %s</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="179"/>
+        <source>Something is wrong. Can&apos;t change geometry.</source>
+        <translation>Er is een probleem. Kan de geometrie niet aanpassen.</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="217"/>
+        <source>Select a target feature!</source>
+        <translation>Selecteer een doelobject!</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="220"/>
+        <source>Start editing a vector layer!</source>
+        <translation>Start met het bewerken van een vectorlaag!</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="225"/>
+        <source>Copy feature with the geometry you need to paste first!</source>
+        <translation>Kopieer eerst het object met de geometrie die u wil plakken!</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="114"/>
+        <source>Geometry Paster</source>
+        <translation>Geometrie Plakker</translation>
+    </message>
+    <message>
+        <location filename="../plugin.py" line="94"/>
+        <source>About plugin…</source>
+        <translation>Over de plugin…</translation>
+    </message>
+</context>
+</TS>
