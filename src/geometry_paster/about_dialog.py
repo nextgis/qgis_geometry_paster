0d004925ca8578e1ba1fe731592c82f61643ba94
Valeriy Remeshevskiy <remeshevskiy@gmail.com>
2024-10-25 18:02:34 +0300

    Update about dialog

--- src/geometry_paster/about_dialog.py
+++ src/geometry_paster/about_dialog.py
@@ -1,15 +1,25 @@
+from enum import IntEnum
 from pathlib import Path
 from typing import Dict, Optional
 
 from qgis.core import QgsSettings
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import QLocale
-from qgis.PyQt.QtWidgets import QDialog, QWidget
+from qgis.PyQt.QtCore import QLocale, QSize, Qt, pyqtSlot
+from qgis.PyQt.QtGui import QPixmap
+from qgis.PyQt.QtSvg import QSvgWidget
+from qgis.PyQt.QtWidgets import QDialog, QLabel, QWidget
 from qgis.utils import pluginMetadata
 
 FORM_CLASS, _ = uic.loadUiType(
-    str(Path(__file__).parent / "ui/about_dialog_base.ui")
+    str(Path(__file__).parent / "ui" / "about_dialog_base.ui")
 )
+
+
+class AboutTab(IntEnum):
+    Information = 0
+    License = 1
+    Components = 2
+    Contributors = 3
 
 
 class AboutDialog(QDialog, FORM_CLASS):
@@ -18,11 +28,89 @@ class AboutDialog(QDialog, FORM_CLASS):
         self.setupUi(self)
         self.__package_name = package_name
 
-        metadata = self.__metadata()
+        self.tab_widget.setCurrentIndex(0)
 
-        self.pluginName.setText(self.pluginName.text().format_map(metadata))
-        self.setWindowTitle(self.windowTitle().format_map(metadata))
-        self.textBrowser.setHtml(self.__html(metadata))
+        self.support_us_button.clicked.connect(self.__support_us)
+        self.support_us_button.hide()
+
+        metadata = self.__metadata()
+        self.__set_icon(metadata)
+        self.__fill_headers(metadata)
+        self.__fill_about(metadata)
+        self.__fill_license()
+        self.__fill_components()
+        self.__fill_contributors()
+
+    @pyqtSlot()
+    def __support_us(self) -> None:
+        pass
+
+    def __fill_headers(self, metadata: Dict[str, Optional[str]]) -> None:
+        plugin_name = metadata["plugin_name"]
+        assert isinstance(plugin_name, str)
+        if "NextGIS" not in plugin_name:
+            plugin_name += self.tr(" by NextGIS")
+
+        self.setWindowTitle(self.windowTitle().format(plugin_name=plugin_name))
+        self.plugin_name_label.setText(
+            self.plugin_name_label.text().format_map(metadata)
+        )
+        self.version_label.setText(
+            self.version_label.text().format_map(metadata)
+        )
+
+    def __set_icon(self, metadata: Dict[str, Optional[str]]) -> None:
+        if metadata.get("icon_path") is None:
+            return
+
+        header_size: QSize = self.info_layout.sizeHint()
+
+        icon_path = Path(__file__).parent / str(metadata.get("icon_path"))
+        svg_icon_path = icon_path.with_suffix(".svg")
+
+        if svg_icon_path.exists():
+            icon_widget: QWidget = QSvgWidget(str(svg_icon_path), self)
+            icon_size: QSize = icon_widget.sizeHint()
+        else:
+            pixmap = QPixmap(str(icon_path))
+            if pixmap.size().height() > header_size.height():
+                pixmap = pixmap.scaled(
+                    header_size.height(),
+                    header_size.height(),
+                    Qt.AspectRatioMode.KeepAspectRatioByExpanding,
+                )
+
+            icon_size: QSize = pixmap.size()
+
+            icon_widget = QLabel(self)
+            icon_widget.setPixmap(pixmap)
+            icon_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        icon_size.scale(
+            header_size.height(),
+            header_size.height(),
+            Qt.AspectRatioMode.KeepAspectRatioByExpanding,
+        )
+        icon_widget.setFixedSize(icon_size)
+        self.header_layout.insertWidget(0, icon_widget)
+
+    def __fill_about(self, metadata: Dict[str, Optional[str]]) -> None:
+        self.about_text_browser.setHtml(self.__html(metadata))
+
+    def __fill_license(self) -> None:
+        license_path = Path(__file__).parent / "LICENSE"
+        if not license_path.exists():
+            self.tab_widget.setTabVisible(AboutTab.License, False)
+            return
+
+        self.tab_widget.setTabVisible(AboutTab.License, True)
+        self.license_text_browser.setPlainText(license_path.read_text())
+
+    def __fill_components(self) -> None:
+        self.tab_widget.setTabVisible(AboutTab.Components, False)
+
+    def __fill_contributors(self) -> None:
+        self.tab_widget.setTabVisible(AboutTab.Contributors, False)
 
     def __locale(self) -> str:
         override_locale = QgsSettings().value(
@@ -50,10 +138,12 @@ class AboutDialog(QDialog, FORM_CLASS):
         about = metadata_value("about")
         assert about is not None
         for about_stop_phrase in (
-            "Разработан компанией",
+            "Разработан",
             "Developed by",
             "Développé par",
-            "Entwickelt von",
+            "Desarrollado por",
+            "Sviluppato da",
+            "Desenvolvido por",
         ):
             if about.find(about_stop_phrase) > 0:
                 about = about[: about.find(about_stop_phrase)]
@@ -62,11 +152,13 @@ class AboutDialog(QDialog, FORM_CLASS):
 
         return {
             "plugin_name": metadata_value("name"),
+            "version": metadata_value("version"),
+            "icon_path": metadata_value("icon"),
             "description": metadata_value("description"),
             "about": about,
             "authors": metadata_value("author"),
             "video_url": metadata_value("video"),
-            "homepage_url": metadata_value("repository"),
+            "homepage_url": metadata_value("homepage"),
             "tracker_url": metadata_value("tracker"),
             "main_url": url,
             "data_url": url.replace("://", "://data."),
